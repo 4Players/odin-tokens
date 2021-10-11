eaf803391e8f906a5fdcaf79d886efd1bc087587
Maximilian Münchow <Maximilian.Muenchow@4players.de>
2021-10-11 16:04:58 +0200

    32 not 33

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ export function generateAccessKey(): string {
   // seed is 32byte long, 31 of them are random.
   key.set(nacl.randomBytes(31), 1);
   // and the last one is a checksum.
-  key[32] = crc8(key.subarray(1, 33));
+  key[32] = crc8(key.subarray(1, 32));
   return Base64.fromUint8Array(key);
 }
 
